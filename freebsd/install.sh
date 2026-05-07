0ddff341f524808e1603d0b96a9442d373b29efb
Charlie Root <root@habench1.my.domain>
2026-05-07 13:18:23 +0300

    wip

--- freebsd/install.sh
+++ freebsd/install.sh
@@ -6,8 +6,9 @@ service cron onestop
 service cron disable
 
 [ ! -d /usr/local/etc/nginx -a ! -h /usr/local/etc/nginx ] && ln -s /root/ha-profile-bench/freebsd/etc/nginx /usr/local/etc/nginx
+[ ! -h /usr/local/etc/haproxy.conf ] && ln -s /root/ha-profile-bench/freebsd/etc/haproxy.conf /usr/local/etc/haproxy.conf 
 
-pkg install -y haproxy nginx toxiproxy-cli toxiproxy-server curl httping gmake libepoll-shim gawk
+pkg install -y haproxy nginx toxiproxy-cli toxiproxy-server curl httping gmake libepoll-shim gawk socat
 
 service nginx enable
 service haproxy enable
@@ -24,4 +25,10 @@ fi
 service nginx restart
 service haproxy restart
 
+echo "NGINX"
+echo
 curl http://127.0.0.1:8080/index.html
+echo
+echo "HAPROXY"
+curl http://127.0.0.1/index.html
+echo
