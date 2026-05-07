159915f2d31fa2a4ca5ac4a601166a0a3e623a56
Charlie Root <root@habench1.my.domain>
2026-05-07 09:58:14 +0300

    wip

--- freebsd/install.sh
+++ freebsd/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+service syslogd onestop
+service syslogd disable
+service cron onestop
+service cron disable
+
 [ ! -d /usr/local/etc/nginx -a ! -h /usr/local/etc/nginx ] && ln -s /root/ha-profile-bench/freebsd/etc/nginx /usr/local/etc/nginx
 
 pkg install -y haproxy nginx toxiproxy-cli toxiproxy-server curl httping gmake libepoll-shim gawk
@@ -7,8 +12,8 @@ pkg install -y haproxy nginx toxiproxy-cli toxiproxy-server curl httping gmake l
 service nginx enable
 service haproxy enable
 
-service toxiproxy-server enable
-service toxiproxy-server start
+#service toxiproxy-server enable
+#service toxiproxy-server start
 
 if [ ! -x /usr/local/bin/h1load ]; then
 	gmake -C /root/ha-profile-bench/h1load
