e96302b8da04bb738c66679ace3ae5b1710dd04e
Super User <root@habench2.my.domain>
2026-05-19 16:21:25 +0300

    wip

--- freebsd/scripts/dtrace.sh
+++ freebsd/scripts/dtrace.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+cat <<EOF
+
+
+Info/howto:
+
+# Oracle:
+#sudo dnf install -y perl-open
+
+cd /root
+git clone https://github.com/brendangregg/FlameGraph.git
+cd FlameGraph
+./flamegraph.pl /tmp/haproxy_collapsed.txt > /tmp/haproxy_flamegraph.svg
+./stackcollapse.pl /tmp/haproxy_stacks.txt | ./flamegraph.pl > /tmp/haproxy_flamegraph.svg
+
+
+EOF
+
+
 dtrace -n '
 profile-997 /execname == "haproxy"/ {
     @[ustack(100), stack()] = count();
